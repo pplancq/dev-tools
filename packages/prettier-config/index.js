fb33607e4a11119a79142d3cebb3da54bac5530d
pplancq <paul.plancq@outlook.fr>
2023-12-12 11:13:41 +0100

    feat(prettier-config): add prettier rule for Dockerfile

--- packages/prettier-config/index.js
+++ packages/prettier-config/index.js
@@ -3,6 +3,16 @@ const config = {
   singleQuote: true,
   arrowParens: 'avoid',
   printWidth: 120,
+  overrides: [
+    {
+      files: ['Dockerfile'],
+      options: {
+        plugins: ['prettier-plugin-sh'],
+        parser: 'sh',
+        indent: 4,
+      },
+    },
+  ],
 };
 
 module.exports = config;
