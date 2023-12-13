c70823a1b457612f744df055d67ef690531c43f9
pplancq <paul.plancq@outlook.fr>
2023-12-13 14:40:57 +0100

    feat(eslint-config): add base rule for react with jsx-a11y ans testing-library for test

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -1,3 +1,3 @@
 module.exports = {
-  extends: ['./rules/base.js', './rules/typescript.js', './rules/prettier.js'].map(require.resolve),
+  extends: ['./rules/base.js', './rules/typescript.js', './rules/prettier.js', './rules/react.js'].map(require.resolve),
 };
---
+++ packages/eslint-config/rules/react.js
@@ -0,0 +1,117 @@
+module.exports = {
+  extends: [
+    'airbnb',
+    'airbnb/hooks',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    './base.js',
+    './typescript.js',
+  ],
+  rules: {
+    // eslint-plugin-import https://github.com/import-js/eslint-plugin-import
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+      },
+    ],
+
+    // eslint-plugin-react https://github.com/jsx-eslint/eslint-plugin-react
+    'react/no-multi-comp': [
+      'error',
+      {
+        ignoreStateless: false,
+      },
+    ],
+    'react/jsx-props-no-spreading': 'off',
+    'react/no-unescaped-entities': 'off',
+    'react/jsx-filename-extension': [
+      'error',
+      {
+        extensions: ['.jsx'],
+      },
+    ],
+    'react/function-component-definition': [
+      'error',
+      {
+        namedComponents: ['function-declaration', 'arrow-function', 'function-expression'],
+        unnamedComponents: ['arrow-function', 'function-expression'],
+      },
+    ],
+
+    // eslint-plugin-react-hooks https://github.com/facebook/react/tree/main/packages/eslint-plugin-react-hook
+    'react-hooks/exhaustive-deps': 'warn',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx'],
+      },
+    },
+  },
+  overrides: [
+    {
+      files: ['**/*.ts?(x)'],
+      settings: {
+        'import/resolver': {
+          node: {
+            extensions: ['.js', '.jsx', '.ts', '.tsx'],
+          },
+        },
+      },
+      rules: {
+        // eslint-plugin-import https://github.com/import-js/eslint-plugin-import
+        'import/extensions': [
+          'error',
+          'ignorePackages',
+          {
+            js: 'never',
+            jsx: 'never',
+            ts: 'never',
+            tsx: 'never',
+          },
+        ],
+
+        // eslint-plugin-react https://github.com/jsx-eslint/eslint-plugin-react
+        'react/require-default-props': 'off',
+        'react/jsx-filename-extension': [
+          'error',
+          {
+            extensions: ['.jsx', '.tsx'],
+          },
+        ],
+      },
+    },
+    {
+      extends: ['plugin:jest-dom/recommended'],
+      files: ['*.{test,spec}.{js,jsx,ts,tsx}'],
+      plugins: ['testing-library'],
+      rules: {
+        // eslint-plugin-testing-library https://github.com/testing-library/eslint-plugin-testing-library
+        'testing-library/await-async-queries': 'error',
+        'testing-library/await-async-utils': 'error',
+        'testing-library/no-await-sync-queries': 'error',
+        'testing-library/no-container': 'error',
+        'testing-library/no-debugging-utils': 'error',
+        'testing-library/no-dom-import': ['error', 'react'],
+        'testing-library/no-node-access': 'error',
+        'testing-library/no-promise-in-fire-event': 'error',
+        'testing-library/no-render-in-lifecycle': 'error',
+        'testing-library/no-unnecessary-act': 'error',
+        'testing-library/no-wait-for-multiple-assertions': 'error',
+        'testing-library/no-wait-for-side-effects': 'error',
+        'testing-library/no-wait-for-snapshot': 'error',
+        'testing-library/prefer-find-by': 'error',
+        'testing-library/prefer-presence-queries': 'error',
+        'testing-library/prefer-query-by-disappearance': 'error',
+        'testing-library/prefer-screen-queries': 'error',
+        'testing-library/render-result-naming-convention': 'error',
+      },
+    },
+  ],
+};
