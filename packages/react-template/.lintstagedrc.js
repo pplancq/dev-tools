bd8c1d3b75b0c83ed36982afd58420293d6b47a7
pplancq <paul.plancq@outlook.fr>
2024-02-12 19:44:37 +0100

    chore(react-template): add check package on commit check

--- packages/react-template/.lintstagedrc.js
+++ packages/react-template/.lintstagedrc.js
@@ -2,4 +2,5 @@ module.exports = {
   '*.{js,jsx,ts,tsx}': 'eslint --fix',
   '*.{ts,tsx}': 'tsc-files --noEmit',
   '*.{scss,css}': 'stylelint --fix',
+  '(package|package-lock).json': 'npm run package:check',
 };
