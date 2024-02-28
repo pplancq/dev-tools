625247ebb7e86be8c540aeb5f7d64f9b535bc0f2
pplancq <paul.plancq@outlook.fr>
2024-02-28 20:03:18 +0100

    feat(react-template): add fetchApi example

--- packages/react-template/src/api/demoApi.ts
+++ packages/react-template/src/api/demoApi.ts
@@ -1,44 +1,10 @@
+import { fetchApi } from '@Front/api/fetchApi';
 import type { Users } from '@Front/types/demoApi';
 
-const data: Users = [
-  {
-    id: 1,
-    firstName: 'John',
-    lastName: 'Doe',
-    age: 30,
-    email: 'john.doe@example.com',
-  },
-  {
-    id: 2,
-    firstName: 'Jane',
-    lastName: 'Smith',
-    age: 25,
-    email: 'jane.smith@example.com',
-  },
-  {
-    id: 3,
-    firstName: 'Alice',
-    lastName: 'Johnson',
-    age: 28,
-    email: 'alice.johnson@example.com',
-  },
-  {
-    id: 4,
-    firstName: 'Bob',
-    lastName: 'Brown',
-    age: 35,
-    email: 'bob.brown@example.com',
-  },
-];
-
-export const fetchDemoApi = async (): Promise<Users> => {
-  return new Promise<Users>((resolve, reject) => {
-    try {
-      setTimeout(() => {
-        resolve(data);
-      }, 3000);
-    } catch (error) {
-      reject(error);
-    }
+export const fetchDemoApi = async () => {
+  const users = await fetchApi<Users>({
+    path: 'https://jsonplaceholder.typicode.com/users',
   });
+
+  return users.map(user => ({ ...user, username: user.username.toUpperCase(), email: user.email.toLowerCase() }));
 };
