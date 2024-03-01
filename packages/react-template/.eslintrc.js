0e8813ae02ffc43d3da292315f37e90620f7b071
pplancq <paul.plancq@outlook.fr>
2024-03-01 19:24:10 +0100

    feat(react-template): update eslint un stylelint config add peerdeps of eslint stylelint and prettier config

--- packages/react-template/.eslintrc.js
+++ packages/react-template/.eslintrc.js
@@ -1,1 +1,1 @@
-module.exports = { extends: ['@pplancq/eslint-config/react', '@pplancq/eslint-config/vitest'] };
+module.exports = { extends: ['@pplancq/eslint-config/react', '@pplancq/eslint-config/vitest', '@pplancq/eslint-config/prettier'] };
