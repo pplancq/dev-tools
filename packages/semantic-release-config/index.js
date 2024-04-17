c94b9af7eac1c42fc31e7ff5971e96ef435d3068
pplancq <paul.plancq@outlook.fr>
2024-04-17 18:10:42 +0200

    revert(semantic-release-config): add type of commit chore, docs, style, refactor, perf and test to patch release
    This reverts commit 6f84187f2a754b11a91dc2c3af31af548483d444.

--- packages/semantic-release-config/index.js
+++ packages/semantic-release-config/index.js
@@ -24,17 +24,6 @@ const config = {
       '@semantic-release/commit-analyzer',
       {
         preset: 'conventionalcommits',
-        releaseRules: [
-          { breaking: true, release: 'major' },
-          { type: 'feat', release: 'minor' },
-          { type: 'fix', release: 'patch' },
-          { type: 'chore', release: 'patch' },
-          { type: 'docs', release: 'patch' },
-          { type: 'style', release: 'patch' },
-          { type: 'refactor', release: 'patch' },
-          { type: 'perf', release: 'patch' },
-          { type: 'test', release: 'patch' },
-        ],
         parserOpts: {
           noteKeywords: ['BREAKING CHANGE', 'BREAKING CHANGES'],
         },
