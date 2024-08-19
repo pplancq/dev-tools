037c9081ca212979a3c053ce8c84ea18da016756
pplancq <paul.plancq@outlook.fr>
2024-08-19 11:42:28 +0200

    feat(prettier-config): add plugin for gherkin, nginx and properties files

--- packages/prettier-config/index.js
+++ packages/prettier-config/index.js
@@ -3,13 +3,25 @@ const config = {
   singleQuote: true,
   arrowParens: 'avoid',
   printWidth: 120,
+  plugins: ['prettier-plugin-sh', 'prettier-plugin-gherkin', 'prettier-plugin-nginx', 'prettier-plugin-properties'],
   overrides: [
     {
       files: ['Dockerfile'],
       options: {
-        plugins: ['prettier-plugin-sh'],
         parser: 'sh',
         indent: 4,
+      },
+    },
+    {
+      files: ['nginx.conf'],
+      options: {
+        parser: 'nginx',
+      },
+    },
+    {
+      files: '*.properties',
+      options: {
+        printWidth: 0,
       },
     },
   ],
