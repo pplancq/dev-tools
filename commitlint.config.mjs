4172586af9af7100603fb09ba8224c393cb6bc03
pplancq <paul.plancq@outlook.fr>
2024-11-12 19:40:37 +0100

    chore(dev-tools): use commitlint for limit scope

---
+++ commitlint.config.mjs
@@ -0,0 +1,25 @@
+// eslint-disable-next-line import/no-default-export
+export default {
+  extends: ['@pplancq/commitlint-config'],
+  rules: {
+    'scope-enum': [
+      2,
+      'always',
+      [
+        'dev-tools',
+        'babel-config',
+        'commitlint-config',
+        'create-react-app',
+        'eslint-config',
+        'postcss-config',
+        'prettier-config',
+        'react-template',
+        'semantic-release-config',
+        'stylelint-config',
+        'webpack-config',
+        'deps',
+        'release',
+      ],
+    ],
+  },
+};
