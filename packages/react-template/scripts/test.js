05f0205e81c17e8c9b5cb6a90358d3de3f954a9e
pplancq <paul.plancq@outlook.fr>
2025-06-08 12:39:16 +0200

    feat(react-template): update test scripts and add concurrent execution for unit and e2e tests

---
+++ packages/react-template/scripts/test.js
@@ -0,0 +1,20 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import concurrently from 'concurrently';
+
+const args = process.argv.slice(2);
+
+let commands = ['npm:test:unit', 'npm:test:e2e'];
+
+if (args.includes('--coverage')) {
+  commands = commands.map(command => `${command} -- --coverage`);
+}
+
+const { result } = concurrently(commands, {
+  prefixColors: 'auto',
+  maxProcesses: 1,
+});
+
+result.then(
+  () => process.exit(0),
+  () => process.exit(1),
+);
