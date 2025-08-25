ed578822fb89ba261554a66d7b11804c7ff81abc
Paul PLANCQ <paul.plancq@outlook.fr>
2025-08-25 10:25:01 +0200

    feat(react-template): add @msw/playwright fixture support for tests

--- packages/react-template/tests/fixture/playwright.fixture.ts
+++ packages/react-template/tests/fixture/playwright.fixture.ts
@@ -1,10 +1,13 @@
+import { createNetworkFixture, type NetworkFixture } from '@msw/playwright';
 import { test as base } from '@playwright/test';
 import { createTestWithCoverageFixture } from '../utils/createTestWithCoverageFixture';
 
 export const test = base.extend<{
   testWithCoverage: string;
+  network: NetworkFixture;
 }>({
   testWithCoverage: createTestWithCoverageFixture,
+  network: createNetworkFixture(),
 });
 
 export const it = test;
