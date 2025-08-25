69494cc5beb0f1e12df43f883e2a6dcd90afc350
Paul PLANCQ <paul.plancq@outlook.fr>
2025-08-25 14:13:01 +0200

    test(create-react-app): add vitest configuration and testing scripts
    Integrate vitest for testing with new scripts for running tests and watching changes.
    Add coverage reporting and configure test environment settings.

---
+++ packages/create-react-app/vitest.config.ts
@@ -0,0 +1,38 @@
+import { loadEnv } from 'vite';
+import viteTsconfigPaths from 'vite-tsconfig-paths';
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [viteTsconfigPaths()],
+    test: {
+      setupFiles: 'vitest.setup.ts',
+      clearMocks: true,
+      reporters: ['default', 'junit', 'vitest-sonar-reporter'],
+      outputFile: {
+        'vitest-sonar-reporter': 'sonar-report.xml',
+        junit: 'junit-report.xml',
+      },
+      include: ['tests/**/*.{test,spec}.[jt]s?(x)'],
+      poolOptions: {
+        forks: {
+          minForks: env.CI ? 1 : undefined,
+          maxForks: env.CI ? 2 : undefined,
+        },
+      },
+      coverage: {
+        enabled: env.CI === 'true',
+        reporter: ['lcovonly', 'html', 'text', 'text-summary'],
+        provider: 'v8',
+        lines: 80,
+        functions: 75,
+        branches: 80,
+        statements: 80,
+        include: ['src/**/*.[jt]s?(x)'],
+        exclude: ['src/**/*.d.[jt]s?(x)', 'src/**/*.types.[jt]s?(x)', 'src/**/index.[jt]s?(x)'],
+      },
+    },
+  };
+});
