5132851f4459b9494f17922e1a24c424d7ba4d46
Paul PLANCQ <paul.plancq@outlook.fr>
2025-08-26 08:34:36 +0200

    chore(dev-tools): add eslint configuration for prettier integration

--- scripts/lint.js
+++ scripts/lint.js
@@ -1,7 +1,7 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 const concurrently = require('concurrently');
 
-const { result } = concurrently(['npm:lint:eslint', 'npm:lint:tsc', 'npm:lint:stylelint', 'npm:lint:prettier'], {
+const { result } = concurrently(['npm:lint:*(!fix)'], {
   prefixColors: 'auto',
   maxProcesses: process.env.CI ? 1 : undefined,
 });
