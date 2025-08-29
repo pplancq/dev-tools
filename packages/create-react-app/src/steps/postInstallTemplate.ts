851defcd935820a863e2efe8bc22c452710ffac6
Paul PLANCQ <paul.plancq@outlook.fr>
2025-08-29 10:57:53 +0200

    feat(react-template): add post-install script check and warning

--- packages/create-react-app/src/steps/postInstallTemplate.ts
+++ packages/create-react-app/src/steps/postInstallTemplate.ts
@@ -1,10 +1,19 @@
 import { RunCommandError } from '@/Error/RunCommandError';
 import { runCommand } from '@/helpers/runCommand';
 import { log, spinner } from '@clack/prompts';
+import { readFileSync } from 'node:fs';
+import { resolve } from 'node:path';
 
 export const postInstallTemplate = async (projectName: string, repoDir: string) => {
   const spinnerInstance = spinner();
   let hasError = false;
+
+  const templatePackageJson = JSON.parse(readFileSync(resolve(repoDir, 'package.json'), { encoding: 'utf-8' }));
+
+  if (templatePackageJson?.bin?.['template-install'] === undefined) {
+    log.warn('No post install template script found.');
+    return;
+  }
 
   spinnerInstance.start('Running post install template script...');
   try {
