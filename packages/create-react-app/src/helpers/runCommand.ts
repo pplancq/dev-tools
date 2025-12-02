a30988c6b0954c6067cf8efd555882655c259c75
Paul PLANCQ <paul.plancq@outlook.fr>
2025-12-02 17:45:10 +0100

    fix(create-react-app): adjust shell option for cross-platform compatibility
    Update the shell option in runCommand to use platform-specific settings, ensuring compatibility across different operating systems. Additionally, correct the commit message format in gitCommit to prevent issues with initial commit messages.

--- packages/create-react-app/src/helpers/runCommand.ts
+++ packages/create-react-app/src/helpers/runCommand.ts
@@ -1,6 +1,7 @@
 import { RunCommandError } from '@/Error/RunCommandError';
 import type { ExecFileOptions } from 'child_process';
 import { execFile } from 'node:child_process';
+import { platform } from 'node:process';
 import { promisify } from 'node:util';
 
 const execFileAsync = promisify(execFile);
@@ -8,7 +9,7 @@ const execFileAsync = promisify(execFile);
 export const runCommand = async (command: string, args: string[] = [], options?: ExecFileOptions) => {
   try {
     const execOptions: ExecFileOptions = {
-      shell: true,
+      shell: platform === 'win32',
       ...options,
     };
     await execFileAsync(command, args, execOptions);
