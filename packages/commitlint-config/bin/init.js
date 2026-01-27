0951ec2abaa89e2d28f3a47faeb446d078ffb838
Paul PLANCQ <paul.plancq@outlook.fr>
2026-01-27 14:37:45 +0100

    feat(commitlint-config)!: migrate to ESM and update configuration files
    Refactor the Commitlint configuration to use ECMAScript modules (ESM) instead of CommonJS.
    Rename configuration files and update import/export syntax accordingly.
    
    BREAKING CHANGE: Drop CommonJS support. See the [README](https://github.com/pplancq/dev-tools/blob/main/packages/commitlint-config/README.md) for migration instructions.

--- packages/commitlint-config/bin/init.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-const { writeFileSync } = require('fs');
-
-console.info('Add commitlint config in .commitlintrc.js');
-
-writeFileSync('.commitlintrc.js', "module.exports = { extends: ['@pplancq/commitlint-config'] };\n", {
-  encoding: 'utf-8',
-});
--- packages/commitlint-config/index.js
+++ packages/commitlint-config/index.js
@@ -9,4 +9,5 @@ const config = {
   },
 };
 
-module.exports = config;
+// eslint-disable-next-line import/no-default-export
+export default config;
