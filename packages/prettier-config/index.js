fdfd2c317aa070979f5569a70138ff2e0a07283f
Paul PLANCQ <paul.plancq@outlook.fr>
2026-07-29 21:47:19 +0200

    feat(prettier-config): remove nginx plugin and clean configuration

--- packages/prettier-config/index.js
+++ packages/prettier-config/index.js
@@ -3,19 +3,13 @@ const config = {
   singleQuote: true,
   arrowParens: 'avoid',
   printWidth: 120,
-  plugins: ['prettier-plugin-sh', 'prettier-plugin-gherkin', 'prettier-plugin-nginx', 'prettier-plugin-properties'],
+  plugins: ['prettier-plugin-sh', 'prettier-plugin-gherkin', 'prettier-plugin-properties'],
   overrides: [
     {
       files: ['Dockerfile'],
       options: {
         parser: 'sh',
         indent: 4,
-      },
-    },
-    {
-      files: ['nginx.conf'],
-      options: {
-        parser: 'nginx',
       },
     },
     {
