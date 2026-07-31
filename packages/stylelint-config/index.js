b7feee5c9c6baa85bab79e5ec01ec4ebea6d7f55
Paul PLANCQ <paul.plancq@outlook.fr>
2026-07-31 17:43:35 +0200

    refactor(stylelint-config): remove dynamic require for rule resolution

--- packages/stylelint-config/index.js
+++ packages/stylelint-config/index.js
@@ -1,8 +1,4 @@
-import { createRequire } from "node:module";
-
-const require = createRequire(import.meta.url);
-
 // eslint-disable-next-line import/no-default-export
 export default {
-  extends: ["./rules/core.js", "./rules/moduleCss.js", "./rules/order.js"].map(require.resolve),
+  extends: ["./rules/core.js", "./rules/moduleCss.js", "./rules/order.js"],
 };
