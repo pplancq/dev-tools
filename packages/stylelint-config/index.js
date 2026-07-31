9aef819e91af1bb745afacf271528ff6978dbe66
Paul PLANCQ <paul.plancq@outlook.fr>
2026-07-31 17:41:17 +0200

    feat(stylelint-config)!: use stylelint-config-standard-scss and restructure rules
    Replace stylelint-config-standard and stylelint-config-recommended-scss dependencies with stylelint-config-standard-scss.
    Split rules into core.js (BEM class pattern) and moduleCss.js (camelCase for CSS modules). Merge the *.module.css and *.module.scss overrides into a single override.
    
    BREAKING CHANGE: class naming convention now enforces BEM for non-module files and camelCase for CSS modules. stylelint-config-standard-scss adds additional SCSS rules that may require code changes in consuming projects.

--- packages/stylelint-config/index.js
+++ packages/stylelint-config/index.js
@@ -4,5 +4,5 @@ const require = createRequire(import.meta.url);
 
 // eslint-disable-next-line import/no-default-export
 export default {
-  extends: ["./rules/baseCss.js", "./rules/baseScss.js", "./rules/order.js"].map(require.resolve),
+  extends: ["./rules/core.js", "./rules/moduleCss.js", "./rules/order.js"].map(require.resolve),
 };
